0dde5bc5b8922742f179d21c5c70d441db948211
NotAlex575 <dapontealessandro89gmail.com>
2025-11-17 16:24:04 +0100

    extra: creato components per la creazione delle singole card

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,12 @@
+
+
+<div class="col-6 col-md-2"> 
+    <div class="card" style="background-color: #1E1E1E;">
+        <img src="{{ $thumb }}" class="card-img-top fixed-card-img" alt="{{ $series }}">
+        <div class="card-body p-2">
+            <h5 class="card-title text-light text-center" style="font-size: 0.9rem;">
+                {{ $series }}
+            </h5>
+        </div>
+    </div>
+</div>
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -8,6 +8,14 @@
 <div style="background-color: #1E1E1E; padding: 2rem;">
     
     @php
+         /*
+
+            <x-card>
+                    <x-slot:thumb>{{ $comic["thumb"] }}</x-slot:thumb>
+                    <x-slot:series>{{ $comic["series"] }}</x-slot:series>
+                </x-card>
+
+        */
         //prendiamo gli array di comics e li salviamo dentro una variabile
         $comics = config("comics");
     @endphp
@@ -15,16 +23,10 @@
     <div class="container">
         <div class="row g-3"> 
             @foreach ($comics as $comic)
-                <div class="col-6 col-md-2"> 
-                    <div class="card" style="background-color: #1E1E1E;">
-                        <img src="{{ $comic['thumb'] }}" class="card-img-top fixed-card-img" alt="{{ $comic['series'] }}">
-                        <div class="card-body p-2">
-                            <h5 class="card-title text-light text-center" style="font-size: 0.9rem;">
-                                {{ $comic['series'] }}
-                            </h5>
-                        </div>
-                    </div>
-                </div>
+                <x-card>
+                    <x-slot:thumb>{{ $comic["thumb"] }}</x-slot:thumb>
+                    <x-slot:series>{{ $comic["series"] }}</x-slot:series>
+                </x-card>
             @endforeach
         </div>
     </div>
